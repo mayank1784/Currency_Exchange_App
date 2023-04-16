662a8c663f0b1942b151573c89a4d0de6a6ff858
Mayank Jain <mayankjain1784@gmail.com>
2023-04-16 19:11:13 +0530

    ejs added

--- app.js
+++ app.js
@@ -1,52 +1,60 @@
 // Import the necessary modules
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
+const https = require("https");
+const ejs = require("ejs");
+const appid = "f5acb51cd0f141c0a19a43a5e5539fda";
+const oxr = require("open-exchange-rate");
+const bodyParser = require("body-parser");
+
 const app = express();
-const https = require('https');
-const appid = 'f5acb51cd0f141c0a19a43a5e5539fda';
-const oxr = require('open-exchange-rate');
- const bodyParser = require("body-parser");
- app.use(express.static(__dirname + '/public')); 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.get('/api/currencies', (req, res) => {
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/api/currencies", (req, res) => {
   // Make the API request to fetch the currencies
-  request('https://openexchangerates.org/api/currencies.json', (error, response, body) => {
+  request(
+    "https://openexchangerates.org/api/currencies.json",
+    (error, response, body) => {
       // If there is no error, send the currencies back to the client
       if (!error && response.statusCode === 200) {
-          res.send(body);
+        res.send(body);
       } else {
-          res.send('Error fetching currencies');
+        res.send("Error fetching currencies");
       }
-  });
+    }
+  );
 });
 
 // Serve the HTML file with the form
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.render("index", {amount: 0.00});
 });
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
   var amount = req.body.Amount;
-    var fromCountry = req.body.fromCountry;
-    var toCountry = req.body.toCountry;
-oxr.set({ app_id: appid });
+  var fromCountry = req.body.fromCountry;
+  var toCountry = req.body.toCountry;
+  oxr.set({ app_id: appid });
 
-let currencies = [fromCountry, toCountry];
-oxr.getExchangeRate(currencies).then(({rates}) => {
-  var rateToExchange = rates[toCountry]/rates[fromCountry];
-  var convertedAmount = (amount/rates[fromCountry])*rates[toCountry];  
-  console.log(rateToExchange);
-  console.log(convertedAmount); // { CAD: 1.342507, EUR: 0.896797}
-  res.send({convertedAmount});
-})
-.catch(err => {
-    console.log('Error :: ',err);
-});
-    
+  let currencies = [fromCountry, toCountry];
+  oxr
+    .getExchangeRate(currencies)
+    .then(({ rates }) => {
+      var rateToExchange = rates[toCountry] / rates[fromCountry];
+      var convertedAmount = (amount / rates[fromCountry]) * rates[toCountry];
+      console.log(rateToExchange);
+      console.log(convertedAmount); // { CAD: 1.342507, EUR: 0.896797}
+      res.render("index", {amount: convertedAmount});
+    })
+    .catch((err) => {
+      console.log("Error :: ", err);
+    });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-console.log(Server is running on port ${port});
+  console.log("Server is running on port " + port);
 });
